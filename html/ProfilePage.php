d962cc16e152564132c7f3aa4aab9a1f33bac0cf
Zach <zwireman@umich.edu>
2023-11-17 19:00:57 -0500

    bio functionality

--- html/ProfilePage.php
+++ html/ProfilePage.php
@@ -5,6 +5,8 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "webarchive";
 
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
 
@@ -18,18 +20,65 @@ if (isset($_GET['id'])) {
 	$id = $_GET['id'];
 }
 
-
+//pull up graduate information:
 $query = "select * from graduates where gradID = $id";
 
 $result = mysqli_query($con, $query);
 
 $graduate = mysqli_fetch_assoc($result);
 
+$gradID = $graduate['gradID'];
 $name = $graduate['name'];
 $gradSemester = $graduate['gradSemester'];
 $gradYear = $graduate['gradYear'];
 $major = $graduate['major'];
 $photoAddress = $graduate['photoAddress'];
+
+
+//set default bio information
+$bioID = 1;
+$foundContactInfo = "alumni@email.com";
+$foundMiscInfo = "Current occupation, any miscellaneous accomplishments, general career.";
+
+$query = "select * from bios where gradID = $id and approvalStatus = 1";
+$result = mysqli_query($con, $query);
+
+//find the bio table entry with the highest bioID
+if ($bio = mysqli_fetch_assoc($result)) {
+	while ($bioResult = mysqli_fetch_assoc($result)) {
+		if ($bioResult['bioID'] > $bio['bioID']) {
+			$bio = $bioResult;
+		}
+	}
+	
+	//found a bio with the highest ID
+	$bioID = $bio['bioID'];
+	$foundContactInfo = $bio['contactInfo'];
+	$foundMiscInfo = $bio['miscInfo'];
+}
+
+
+$query = "";
+
+if (isset($_GET['contactInfo'])) {
+	$contactInfo = $_GET['contactInfo'];
+	$miscInfo = $_GET['miscInfo'];
+	$query = "insert into bios (gradID, contactInfo, miscInfo, approvalStatus) values ('$gradID', '$contactInfo', '$miscInfo', 0);";
+}
+
+if (isset($_GET['reportInfo'])) {
+	//$miscInfo = $_GET['miscInfo'];
+	//$query = "insert into reports (gradID, contactInfo, miscInfo, approvalStatus) values ('$gradID', '$contactInfo', '$miscInfo', 0);";
+}
+
+if ($query != "") {
+	echo $query;
+	$result = mysqli_query($con, $query);
+}
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -63,10 +112,11 @@ $photoAddress = $graduate['photoAddress'];
 			</div>
 			<div class= "contactInfo"></div>
 				<ul id="contactList">
-					<li>alumni@email.com</li>
+					<li><?php echo $foundContactInfo; ?></li>
 				</ul>
+				
 			<div class = "miscInfo">
-				<p>Bio: Current occupation, any miscellaneous accomplishments, general career.</p>
+				<p>Bio: <?php echo $foundMiscInfo; ?></p>
 			</div>
 			<button id="editButton" onClick="openEditForm()">Edit Bio</button>
 			<button id="reportButton" onClick="openReportForm()">Report</button>
@@ -80,18 +130,19 @@ $photoAddress = $graduate['photoAddress'];
 	<div class = "popup-content">
 		<button type="button" id="closePopup" onClick="closeEditForm()">&times;</button>
 		<h2>Edit Profile</h2>
-		<form id = "editForm">
+		<form action="profilepage.php" id="editForm" method="get">
+			<input type='hidden' name='id' value='<?php echo $id;?>'>
 			<label for="contactInfo">Contact Information:</label>
 			<br>
-			<textarea id="contactInfo">alumni@email.com</textarea>
+			<textarea id="contactInfo" name="contactInfo" form="editForm">alumni@email.com</textarea>
 			
 			<br>
 			<label for="miscInfo">General Information:</label>
 			<br>
-			<textarea id=miscellaneousInformation name=miscInfo>Bio: Current occupation, any miscellaneous accomplishments, general career.</textarea>
+			<textarea id="miscellaneousInformation" name="miscInfo" form="editForm">Bio: Current occupation, any miscellaneous accomplishments, general career.</textarea>
 			<br>
 			
-			<button type = "button" id="submitRevisions">Submit</button>
+			<button type="submit" id="submitRevisions">Submit</button>
 		</form>
 	</div>
 	
@@ -103,13 +154,14 @@ $photoAddress = $graduate['photoAddress'];
 	<div class = "popup-content">
 		<button type="button" id="closePopup" onClick="closeReportForm()">&times;</button>
 		<h2>Report Profile</h2>
-		<form id = "reportForm">
+		<form action="profilepage.php" id="reportForm" method="get">
+			<input type='hidden' name='id' value='<?php echo $id;?>'>
 			<label for="reportInfo">What's Wrong?</label>
 			<br>
-			<textarea id=reportInformation name=reportInfo></textarea>
+			<textarea id="reportInformation" name="reportInfo" form="reportForm"></textarea>
 			<br>
 			
-			<button type = "button" id="submitReport">Submit</button>
+			<button type="submit" id="submitReport">Submit</button>
 		</form>
 	</div>
 	</div>
